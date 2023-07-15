02f1be987040e530d77ff58160624c90c5a99bb0
Soumajyoti <soumyabwn3@gmail.com>
2023-07-16 01:11:01 +0530

    horizontal scroll fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={mulish.className}>
+    <html lang="en" className={`${mulish.className} max-w-[100vw] overflow-x-hidden`}>
       <body className={mulish.className}>{children}</body>
     </html>
   )
